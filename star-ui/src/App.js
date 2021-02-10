50455e06f1f9f06fcedf08efb8b4a29e58cdcdfd
Shriram Ramanujam <shriramramanujam@gmail.com>
2021-02-11 05:13:15 +0800

    feat: add routes and layout of app

--- star-ui/src/App.js
+++ star-ui/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login></Login>
+        </Route>
+        <Route exact path="/home">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
