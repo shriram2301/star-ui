af59d7f2109e88f6ed987467990c8fbbee02917d
Shriram Ramanujam <shriramramanujam@gmail.com>
2021-02-11 05:13:55 +0800

    feat: add Home Component

---
+++ star-ui/src/Home.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
+import "./home.css";
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+    </div>
+  );
+};
+
+export default Home;
